4016e5b9b8379c548e2109816fba2e51fbeb6f07
Michael Mandrus <michael.mandrus@grafana.com>
2022-04-27 11:40:38 -0400

    Add initial grpc secrets manager impl and dummy server and client

---
+++ pkg/plugins/backendplugin/secretsmanagerplugin/client/client_main.go
@@ -0,0 +1,81 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package main implements a client for Greeter service.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	pb "github.com/grafana/grafana/pkg/plugins/backendplugin/secretsmanagerplugin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
+
+func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewRemoteSecretsManagerClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	res, err := c.Get(ctx, &pb.SecretsRequest{
+		OrgId:     69,
+		Namespace: "random ns",
+		Type:      "random type",
+	})
+	fmt.Println("GET response", res)
+	res2, err := c.Set(ctx, &pb.SecretsRequest{
+		OrgId:     69,
+		Namespace: "random ns",
+		Type:      "random type",
+	})
+	fmt.Println("SET response", res2)
+	res3, err := c.Del(ctx, &pb.SecretsRequest{
+		OrgId:     69,
+		Namespace: "random ns",
+		Type:      "random type",
+	})
+	fmt.Println("DEL response", res3)
+	res4, err := c.Rename(ctx, &pb.SecretsRequest{
+		OrgId:     69,
+		Namespace: "random ns",
+		Type:      "random type",
+	})
+	fmt.Println("RENAME response", res4)
+	res5, err := c.Keys(ctx, &pb.SecretsRequest{
+		OrgId:     69,
+		Namespace: "random ns",
+		Type:      "random type",
+	})
+	fmt.Println("KEYS response", res5)
+}
---
+++ pkg/plugins/backendplugin/secretsmanagerplugin/server/server_main.go
@@ -0,0 +1,92 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package main implements a server for Greeter service.
+package main
+
+import (
+	context "context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/grafana/grafana/pkg/plugins/backendplugin/secretsmanagerplugin"
+	grpc "google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+// server is used to implement secretsmanagerplugin.RemoteSecretsManagerServer.
+type server struct {
+	pb.UnimplementedRemoteSecretsManagerServer
+}
+
+func (s *server) Get(ctx context.Context, sr *pb.SecretsRequest) (*pb.SecretsGetResponse, error) {
+	fmt.Println("received secrets GET request", sr)
+	return &pb.SecretsGetResponse{
+		DecryptedValue: "random bs",
+		Exists:         true,
+		Error:          "",
+	}, nil
+}
+func (s *server) Set(ctx context.Context, sr *pb.SecretsRequest) (*pb.SecretsErrorResponse, error) {
+	fmt.Println("received secrets SET request", sr)
+	return &pb.SecretsErrorResponse{
+		Error: "",
+	}, nil
+}
+func (s *server) Del(ctx context.Context, sr *pb.SecretsRequest) (*pb.SecretsErrorResponse, error) {
+	fmt.Println("received secrets DEL request", sr)
+	return &pb.SecretsErrorResponse{
+		Error: "",
+	}, nil
+}
+func (s *server) Keys(ctx context.Context, sr *pb.SecretsRequest) (*pb.SecretsKeysResponse, error) {
+	fmt.Println("received secrets KEYS request", sr)
+	return &pb.SecretsKeysResponse{
+		Error: "",
+		Keys: []*pb.Key{{
+			OrgId:     69,
+			Namespace: "ns",
+			Type:      "type",
+		}},
+	}, nil
+}
+func (s *server) Rename(ctx context.Context, sr *pb.SecretsRequest) (*pb.SecretsErrorResponse, error) {
+	fmt.Println("received secrets RENAME request", sr)
+	return &pb.SecretsErrorResponse{
+		Error: "",
+	}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterRemoteSecretsManagerServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
