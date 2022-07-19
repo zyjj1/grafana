9e2e51fcff4d16ef43bea8f9a0c01e07e368827c
Jack Westbrook <jack.westbrook@gmail.com>
2022-07-19 16:10:56 +0100

    feat(plugins): introduce moduleLoader to plugin.json for loading via federated modules

--- pkg/api/frontendsettings.go
+++ pkg/api/frontendsettings.go
@@ -37,8 +37,9 @@ func (hs *HTTPServer) getFrontendSettingsMap(c *models.ReqContext) (map[string]i
 	for _, app := range enabledPlugins[plugins.App] {
 		if app.Preload {
 			pluginsToPreload = append(pluginsToPreload, &plugins.PreloadPlugin{
-				Path:    app.Module,
-				Version: app.Info.Version,
+				Path:         app.Module,
+				Version:      app.Info.Version,
+				ModuleLoader: app.ModuleLoader,
 			})
 		}
 	}
@@ -66,6 +67,7 @@ func (hs *HTTPServer) getFrontendSettingsMap(c *models.ReqContext) (map[string]i
 			Name:          panel.Name,
 			Info:          panel.Info,
 			Module:        panel.Module,
+			ModuleLoader:  panel.ModuleLoader,
 			BaseURL:       panel.BaseURL,
 			SkipDataQuery: panel.SkipDataQuery,
 			HideFromList:  panel.HideFromList,
